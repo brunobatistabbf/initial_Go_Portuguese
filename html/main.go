a10a5e3ab1b707c90a1eef491b8aa339755c056a
Bruno Batista <43976638+brunobatistabbf@users.noreply.github.com>
2021-04-14 23:27:49 -0300

    Trabalhando html

---
+++ html/main.go
@@ -0,0 +1,17 @@
+package html
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+func main() {
+	templates = template.Must(template.ParseGlob("*.html"))
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "home.html", nil)
+	})
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
