b0a96075bafeaf7340708150c1fdcc68e7f772a1
Bruno Batista <43976638+brunobatistabbf@users.noreply.github.com>
2021-04-14 22:50:43 -0300

    Estudando HTTP

---
+++ HTTP/http.go
@@ -0,0 +1,26 @@
+package HTTP
+
+import (
+	"log"
+	"net/http"
+)
+
+func main()  {
+	//HTTP é protocolo de comunicação
+	//Base da comunicação web
+	//Cliente <-> Servidor (Request e response)
+
+	//Rotas
+			//URI - identificador do recurso
+			//metodo - GET, POST, PUT, DELETE
+
+	http.HandleFunc("\home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ola mundo"))
+
+	})
+
+	log.Fatal(http.ListenAndServe(";5000", nil))
+
+
+
+}
